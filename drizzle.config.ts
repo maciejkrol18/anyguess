5fc9f63e5bb8ec960bed1e63a1209b4264ac3652
Maciej Król <maciekgplay@gmail.com>
2024-08-12 15:50:45 +0200

    refactor: small drizzle adjustments

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
